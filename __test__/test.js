892116c3a7f62ef963b302d1e3ecbcc3811ae65d
Funbi Oyede <oyedetobi49@gmail.com>
2022-02-02 11:28:47 +0000

    update readme

--- __test__/test.js
+++ __test__/test.js
@@ -15,63 +15,82 @@ const {clearMicroCache,GetNumberOfEvents, Delay} = require('./util/util')
 });
 
 
+describe("/STATUS", () => {
 
-it('Number of good events should be equals 1 when the uptime of the server is checked', async ()=>  {
+  it('Number of good events should be equals 1 and bad events equals 0 when the uptime of the server is checked', async ()=>  {
 
-  const response = await request
-  .get('/status')
-  .set('Accept', 'application/json');
-  expect(response.status).toBe(200);
-
-
-  //delay
-  await Delay(1000)
-
-  const data = await GetNumberOfEvents();
-  expect(data.good).toBe(1);
-});
-
-  it('Number of good events should be equals 1 when list of bookings is fetched', async ()=>  {
-
-        const response = await request
-        .get('/bookings')
-        .set('Accept', 'application/json');
-        expect(response.status).toBe(200);
-
-
-        //delay
-        await Delay(1000)
-
-     const data = await GetNumberOfEvents();
-     expect(data.good).toBe(1);
-});
-
- it('Number of good events should be equals 1 when a booking is made', async ()=>  {
-
-        const response = await request
-        .post('/booking')
-        .set('Accept', 'application/json');
-        expect(response.status).toBe(201);
-        //delay
-        await Delay(1000)
-            
-     const data = await GetNumberOfEvents();
-     expect(data.good).toBe(1);
-});
-
-
-it('Number of good events should be equals 1 when a booking is canceled', async ()=>  {
-
-  const response = await request
-  .delete('/cancel/booking')
-  .set('Accept', 'application/json');
-  expect(response.status).toBe(202);
-  //delay
-  await Delay(1000)
-      
-const data = await GetNumberOfEvents();
-expect(data.good).toBe(1);
-});
+    const response = await request
+    .get('/status')
+    .set('Accept', 'application/json');
+    expect(response.status).toBe(200);
+  
+  
+    //delay
+    await Delay(1000)
+  
+    const data = await GetNumberOfEvents();
+    expect(data.good).toBe(1);
+    expect(data.bad).toBe(0);
+  });
 
   
 
+});
+
+
+describe("/Bookings", () => {
+
+  it('Number of good events should be equals 1 and bad events equals zero when list of bookings is fetched', async ()=>  {
+
+    const response = await request
+    .get('/bookings')
+    .set('Accept', 'application/json');
+    expect(response.status).toBe(200);
+
+
+    //delay
+    await Delay(1000)
+
+ const data = await GetNumberOfEvents();
+ expect(data.good).toBe(1);
+ expect(data.bad).toBe(0);
+});
+
+it('Number of good events should be equals 1 and bad events equals zero when a booking is made', async ()=>  {
+
+    const response = await request
+    .post('/booking')
+    .set('Accept', 'application/json');
+    expect(response.status).toBe(201);
+    //delay
+    await Delay(1000)
+        
+ const data = await GetNumberOfEvents();
+ expect(data.good).toBe(1);
+ expect(data.bad).toBe(0);
+
+});
+
+
+    it('Number of good events should be equals 1 and bad events equals zero when a booking is canceled', async ()=>  {
+
+    const response = await request
+    .delete('/cancel/booking')
+    .set('Accept', 'application/json');
+    expect(response.status).toBe(202);
+    //delay
+    await Delay(1000)
+      
+    const data = await GetNumberOfEvents();
+    expect(data.good).toBe(1);
+    expect(data.bad).toBe(0);
+    });
+
+
+
+
+
+
+ });
+
+  
